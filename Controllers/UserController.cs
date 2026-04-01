7ad3311fc2087ee56819814d2a8900155f7646ac
codervi <mr.asmtylmz@gmail.com>
2026-04-02 00:25:41 +0300

    User View Page Comleted

---
+++ Controllers/UserController.cs
@@ -0,0 +1,43 @@
+﻿using CoderBlog.Models;
+using CoderBlog.Models.DataContext;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoderBlog.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly BlogDbContext _context;
+
+        public UserController(BlogDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        // 2. Formdan gelen veriyi veritabanına kaydeden metot (POST)
+        [HttpPost]
+        public async Task<IActionResult> Register(User user)
+        {
+            // 1. Tarihi biz otomatik veriyoruz
+            user.RegisteredDate = DateTime.Now;
+
+            // 2. ModelState'ten tarih hatasını siliyoruz (Kritik nokta burası!)
+            ModelState.Remove("RegisteredDate");
+
+            if (ModelState.IsValid)
+            {
+                _context.Users.Add(user);
+                await _context.SaveChangesAsync();
+                // Şimdilik ana sayfaya gitsin, Login yapınca oraya bağlarsın
+                return RedirectToAction("Index", "Home");
+            }
+
+            // Eğer hala false dönüyorsa hatayı gör diye sayfaya geri gönderir
+            return View(user);
+        }
+    }
+}
--- Models/User.cs
+++ Models/User.cs
@@ -12,8 +12,11 @@ namespace CoderBlog.Models
         public string Password { get; set; }
         public DateTime RegisteredDate { get; set; } = DateTime.Now;
 
+
         // Bir kullanıcının birden fazla makalesi olabilir (İlişki)
-        public ICollection<BlogPost> BlogPosts { get; set; }
+        public ICollection<BlogPost>? BlogPosts { get; set; }
+        //Not : Yukarıda ki bölüm Required olammasına rağmen başka yerde ki tanımından dolayı DB de kayıt sırasında sorun
+        // yaratıyordu. Bizlerde Soru işareti "?" atarak " Zorunlu değildir " ibaresini burada eklemiş oluyoruz
 
     }
 }
